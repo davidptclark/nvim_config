1a7991e0eb621b28bc25756547d07ea5f84fc3b3
davidpclark <144951807+davidpclark@users.noreply.github.com>
2024-01-03 13:13:57 +0000

    Add bash LSP

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -11,5 +11,4 @@ vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
 vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
 
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
-vim.wo.number = true
-
+vim.wo.relativenumber = true
