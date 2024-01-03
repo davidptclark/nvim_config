f815459491369f32d377d1156e4b37e66c0bceb1
davidpclark <144951807+davidpclark@users.noreply.github.com>
2024-01-03 15:11:17 +0000

    add remaps

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -10,5 +10,11 @@ vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
 vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
 vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
 
+-- Move halfway and center
+vim.keymap.set('n', '<c-d>', '<c-d>zz')
+vim.keymap.set('n', '<c-u>', '<c-u>zz')
+
+-- Remove highlighting
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+
 vim.wo.relativenumber = true
